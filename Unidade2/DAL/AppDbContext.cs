45e1938e60f4638e70c835ba19e22f93ea3104cb
Pedro Resende <pedrohrs08@gmail.com>
2015-08-18 09:14:12 -0300

    first commit

---
+++ Unidade2/DAL/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using AcmeCursos.Models;
+
+namespace AcmeCursos.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("DefaultConnection")
+        {
+            
+        }
+
+        public DbSet<Estudante> Estudantes { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+    }
+}
---
+++ Unidade2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Unidade2.Startup))]
+namespace Unidade2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
